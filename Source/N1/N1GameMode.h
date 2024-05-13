ea9f0cafb9f39f9b5766d0ad560e0f3301ddc7ad
cheonyongjun <yes1312@naver.com>
2024-05-13 18:49:28 +0900

    Init project

---
+++ Source/N1/N1GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "N1GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AN1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AN1GameMode();
+};
+
+
+
