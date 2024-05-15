22b769258b1eb6da1851a18c3f5ce2f1db8ccbe6
cheonyongjun <yes1312@naver.com>
2024-05-16 03:08:36 +0900

    Ability System

---
+++ Source/N1/AbilitySystem/N1AbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "N1AbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class N1_API UN1AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
